a251404192f5afc3bfbee2af1d857a9e19247e0b
DELL <aichhickshukla@gmail.com>
2023-03-26 20:26:03 +0530

    add controllers for user profile

--- routes/index.js
+++ routes/index.js
@@ -4,4 +4,9 @@ const homeController=require('../controller/home_controller');
 console.log('router loaded ');
 
 router.get('/', homeController.home);
+router.use('/users', require('./users'));
+
+
+
+
 module.exports=router;
